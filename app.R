1c5157546c289de2b23b85641ca1108121406864
Austin Arey <a.arey89@gmail.com>
2023-03-30 16:49:50 -0700

    Moved credentials to config

--- app.R
+++ app.R
@@ -21,17 +21,6 @@ source('app_server.R')
 source('app_ui.R')
 source('router.R')
 
-# define some credentials
-credentials <- data.frame(
-  user = c("terry", "Austin"), # mandatory
-  password = c("paintrobot", "Repair1"), # mandatory
-  start = c("2019-04-15"), # optinal (all others)
-  expire = c(NA, "2019-12-31"),
-  admin = c(FALSE, TRUE),
-  comment = "Simple and secure authentification mechanism 
-  for single ‘Shiny’ applications.",
-  stringsAsFactors = FALSE
-)
 
 #Main User Interface
 ui <- function(req) { 
--- exampleConfig.R
+++ exampleConfig.R
@@ -11,3 +11,15 @@ group_list <- c("Group1", "Group2", "Group3")
 tables <- list(pzem580=c("Group1_meter1","Group1_meter2"),
                pzem582=c("Group2_meter1","Group2_meter2"),
                pzemTerry=c("Group3_meter1","Group3_meter2","Group3_meter3"))
+
+# define some credentials
+credentials <- data.frame(
+  user = c("username", "adminuser"), # mandatory
+  password = c("01234", "56789"), # mandatory
+  start = c("2019-04-15"), # optinal (all others)
+  expire = c(NA, "2019-12-31"),
+  admin = c(FALSE, TRUE),
+  comment = "Simple and secure authentification mechanism 
+  for single ‘Shiny’ applications.",
+  stringsAsFactors = FALSE
+)
