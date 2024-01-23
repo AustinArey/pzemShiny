a39a13a0896de576670cb587e37b99d536e24e9e
Austin Arey <austin.arey@viavisolutions.com>
2024-01-23 14:10:25 -0800

    refactor: removed unnecessary group vector

--- app_ui.R
+++ app_ui.R
@@ -1,92 +1,103 @@
 apptheme <- bs_theme(
   version = 5,
-  font_scale = 0.75 #1 #0.75
+  font_scale = 0.75 # 1 #0.75
 )
 
 # Apply Plotly Dark Theme for Dashboards
 plotly_dark <- function(p) {
   a <- list(
-    tickcolor = '#FFFFF'
+    tickcolor = "#FFFFF"
   )
-  pal <- c('white','yellow')
-  p %>% layout(plot_bgcolor  = "rgba(0, 0, 0, 0)",
-               paper_bgcolor = "rgba(0, 0, 0, 0)",
-               fig_bgcolor   = "rgba(0, 0, 0, 0)") %>%
-    layout(font = list(family='Roboto, sans-serif',color = '#FFFFFF')) %>%
+  pal <- c("white", "yellow")
+  p %>%
+    layout(
+      plot_bgcolor = "rgba(0, 0, 0, 0)",
+      paper_bgcolor = "rgba(0, 0, 0, 0)",
+      fig_bgcolor = "rgba(0, 0, 0, 0)"
+    ) %>%
+    layout(font = list(family = "Roboto, sans-serif", color = "#FFFFFF")) %>%
     layout(xaxis = a, yaxis = a)
 }
 
 # Define Group UI
-Group_UI <- function(id) { 
+Group_UI <- function(id) {
   ns <- NS(id)
-  div(id="group",
-      fluidRow(
-        column(2,h4("Group kWh measured:")),
-        column(2,textOutput(ns("totalEnergy"))),
-      ),fluidRow(
-        column(2,h4("Number of days selected:")),
-        column(2,textOutput(ns("daysSelected"))),
-      ),fluidRow(
-        column(2,numericInput(ns("billCost"),label="Bill Total $", value=130.86)),
-        column(2,numericInput(ns("billKwh"),label="Bill Total kWh", value=396 )),
-      ),fluidRow(
-        column(2,h4("= $/Kwh")),
-        column(2,textOutput(ns("costPerKwh"))),
-      ),fluidRow(
-        column(6,div(id="datatable",class="table",
-                     DTOutput(ns('tbl')) %>% withSpinner(color="#0dc5c1"), 
-                     style = "font-size:100%;color:white;background-color:lightgrey;"))
-      )
+  div(
+    id = "group",
+    fluidRow(
+      column(2, h4("Group kWh measured:")),
+      column(2, textOutput(ns("totalEnergy"))),
+    ), fluidRow(
+      column(2, h4("Number of days selected:")),
+      column(2, textOutput(ns("daysSelected"))),
+    ), fluidRow(
+      column(2, numericInput(ns("billCost"), label = "Bill Total $", value = 130.86)),
+      column(2, numericInput(ns("billKwh"), label = "Bill Total kWh", value = 396)),
+    ), fluidRow(
+      column(2, h4("= $/Kwh")),
+      column(2, textOutput(ns("costPerKwh"))),
+    ), fluidRow(
+      column(6, div(
+        id = "datatable", class = "table",
+        DTOutput(ns("tbl")) %>% withSpinner(color = "#0dc5c1"),
+        style = "font-size:100%;color:white;background-color:lightgrey;"
+      ))
+    )
   )
 }
 
 # Live UI #
-LiveStats_UI <- function(id) { 
+LiveStats_UI <- function(id) {
   ns <- NS(id)
-  div(id="live",
-      fluidRow(
-        column(6,div(id="datatable",class="table",DTOutput(ns('liveTable')), 
-                     style = "font-size:100%;color:white;background-color:lightgrey;"))
-      )
+  div(
+    id = "live",
+    fluidRow(
+      column(6, div(
+        id = "datatable", class = "table", DTOutput(ns("liveTable")),
+        style = "font-size:100%;color:white;background-color:lightgrey;"
+      ))
+    )
   )
 }
 
 # Define Meter UI
-Meter_UI <- function(id) { 
+Meter_UI <- function(id) {
   ns <- NS(id)
   sidebarLayout(
     sidebarPanel(
-      #selectInput(ns("group"), "Group", group_list),
-      #dateRangeInput(ns("date_range"), "Date Range", start = Sys.Date() - 30, end = Sys.Date()),
       uiOutput(ns("table_ui")),
-      selectInput(ns("data"), "Data", c("power", "current","voltage","energy")),
-      width = 3),
+      selectInput(ns("data"), "Data", c("power", "current", "voltage", "energy")),
+      width = 3
+    ),
     mainPanel(
-      plotlyOutput(ns("plot")) %>% withSpinner(color="#0dc5c1")
+      plotlyOutput(ns("plot")) %>% withSpinner(color = "#0dc5c1")
     )
   )
 }
 
 
-#function(id) { ns <- NS(id)
-root_ui <- function(id) { 
+# function(id) { ns <- NS(id)
+root_ui <- function(id) {
   ns <- NS(id)
-  div( id="root",class="root",
-       fluidRow(
-         column(6, h3("Electrical Meter Shiny App v0.1"))),
-       fluidRow(
-         column(3,selectInput(ns("group"), "Group", group_list)),
-         column(3,dateRangeInput(ns("date_range"), "Date Range", start = Sys.Date() - 30, end = Sys.Date())),
-       ),
-       tabsetPanel(id=ns("tabsmain"),
-                   #tabPanel("Run View", runview_ui(id)), #Run View tabsetPanel
-                   #tabPanel("Run History", runhist_ui(id)), #Run History tabsetPanel
-                   #tabPanel("Terry's Meters",tags$body(a(href="/?group=terry",class="link"))),
-                   tabPanel("Group Analysis", Group_UI(id)), 
-                   tabPanel("Live Stats", LiveStats_UI(id)),
-                   tabPanel("Meter Charting", Meter_UI(id)),
-       ),
-  )#div root
+  div(
+    id = "root", class = "root",
+    fluidRow(
+      column(6, h3("Electrical Meter Shiny App v0.1"))
+    ),
+    fluidRow(
+      column(3, selectInput(ns("group"), "Group", names(tables))),
+      column(3, dateRangeInput(ns("date_range"), "Date Range", start = Sys.Date() - 30, end = Sys.Date())),
+    ),
+    tabsetPanel(
+      id = ns("tabsmain"),
+      # tabPanel("Run View", runview_ui(id)), #Run View tabsetPanel
+      # tabPanel("Run History", runhist_ui(id)), #Run History tabsetPanel
+      # tabPanel("Terry's Meters",tags$body(a(href="/?group=terry",class="link"))),
+      tabPanel("Group Analysis", Group_UI(id)),
+      tabPanel("Live Stats", LiveStats_UI(id)),
+      tabPanel("Meter Charting", Meter_UI(id)),
+    ),
+  ) # div root
 }
 
 pressure_UI <- function(id) {
--- exampleConfig.R
+++ exampleConfig.R
@@ -1,16 +1,15 @@
-#mysql config
-config_host = 'my.host'
-config_user='user'
-config_password='pw'
-config_dbname='db'
+# mysql config
+config_host <- "my.host"
+config_user <- "user"
+config_password <- "pw"
+config_dbname <- "db"
 
-#meter grouping
-group_list <- c("Group1", "Group2", "Group3")
-
-#tables arranged
-tables <- list(pzem580=c("Group1_meter1","Group1_meter2"),
-               pzem582=c("Group2_meter1","Group2_meter2"),
-               pzemTerry=c("Group3_meter1","Group3_meter2","Group3_meter3"))
+# tables arranged
+tables <- list(
+  group1 = c("group1_meter1", "group1_meter2"),
+  group2 = c("group2_meter1", "group2_meter2"),
+  group3 = c("group3_meter1", "group3_meter2", "group3_meter3")
+)
 
 # define some credentials
 credentials <- data.frame(
@@ -19,7 +18,7 @@ credentials <- data.frame(
   start = c("2019-04-15"), # optinal (all others)
   expire = c(NA, "2019-12-31"),
   admin = c(FALSE, TRUE),
-  comment = "Simple and secure authentification mechanism 
+  comment = "Simple and secure authentification mechanism
   for single ‘Shiny’ applications.",
   stringsAsFactors = FALSE
 )
