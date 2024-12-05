6ff1484284f0d557f576ab3c4df66569b6b5bcc8
FailingSquare60 <a.arey89@gmail.com>
2024-12-05 15:04:46 -0800

    test(group_analysis): updated tests for non-monotonic data

--- tests/testthat.R
+++ tests/testthat.R
@@ -9,8 +9,8 @@ conn <- dbConnect(RSQLite::SQLite(), ":memory:")
 
 # Create mock tables and data
 dbWriteTable(conn, "unitA", data.frame(
-    energy = c(100, 200, 300),
-    date_time = c("2023-01-01 00:00:00", "2023-01-15 00:00:00", "2023-01-31 00:00:00")
+    energy = c(100, 200, 300, 0, 50),
+    date_time = c("2023-01-01 00:00:00", "2023-01-15 00:00:00", "2023-02-01 00:00:00", "2023-02-15 00:00:00", "2023-02-28 00:00:00")
 ))
 
 tables <- list(
@@ -38,8 +38,10 @@ test_that("calc_energy_used works correctly", {
 })
 
 # Unit tests for calc_energy_days_measured
-test_that("calc_energy_days_measured works correctly", {
+test_that("calc_energy_days_measured works monotonic data", {
     date_range <- c("2023-01-01", "2023-02-01")
+    days_selected <- days_diff_num(date_range[1], date_range[2])
+
     initial_query_str <- initial_query("unitA", date_range[1])
     final_query_str <- final_query("unitA", date_range[2])
     max_query_str <- max_query("unitA", date_range[1], date_range[2])
@@ -51,7 +53,26 @@ test_that("calc_energy_days_measured works correctly", {
     result <- calc_energy_days_measured(conn, "unitA", date_range)
     expect_equal(result$table, "unitA")
     expect_equal(result$energy_measured, 200)
-    expect_equal(result$days_measured, 30)
+    expect_equal(result$days_measured, 31)
+})
+
+test_that("calc_energy_days_measured works data that overflowed bufffer", {
+    date_range <- c("2023-01-01", "2023-03-01")
+    days_selected <- days_diff_num(date_range[1], date_range[2])
+    expect_equal(days_selected, 59)
+
+    initial_query_str <- initial_query("unitA", date_range[1])
+    final_query_str <- final_query("unitA", date_range[2])
+    max_query_str <- max_query("unitA", date_range[1], date_range[2])
+
+    initial <- dbGetQuery(conn, initial_query_str)
+    final <- dbGetQuery(conn, final_query_str)
+    max <- dbGetQuery(conn, max_query_str)
+
+    result <- calc_energy_days_measured(conn, "unitA", date_range)
+    expect_equal(result$table, "unitA")
+    expect_equal(result$energy_measured, 250)
+    expect_equal(result$days_measured, 58)
 })
 
 test_that("calc_energy_days_measured handles no data", {
@@ -61,11 +82,11 @@ test_that("calc_energy_days_measured handles no data", {
 
 # Unit tests for create_meter_df
 test_that("create_meter_df works correctly", {
-    date_range <- c("2023-01-01", "2023-01-31")
-    days_selected <- days_diff_num(date_range[1], date_range[2])
+    date_range <- c("2023-01-01", "2023-03-15")
+    days_selected <- days_diff_num(date_range[1], date_range[2]) # 73
     cost_per_kwh <- 0.5
-    expected_kwh <- 0.2
-    expected_days_measured <- 30
+    expected_kwh <- 0.250
+    expected_days_measured <- 58
 
     result <- create_meter_df(conn, tables, "Group 1", date_range, cost_per_kwh, days_selected)
     print("Result:")
@@ -74,7 +95,7 @@ test_that("create_meter_df works correctly", {
     expect_equal(nrow(result), 1)
     expect_equal(result$kWh, expected_kwh)
     expect_equal(result$DaysMeas, expected_days_measured)
-    expect_equal(result$CostEst, expected_kwh * cost_per_kwh * days_selected / result$DaysMeas)
+    expect_equal(result$CostEst, round(expected_kwh * cost_per_kwh * days_selected / result$DaysMeas, 2))
 })
 
 dbDisconnect(conn)
